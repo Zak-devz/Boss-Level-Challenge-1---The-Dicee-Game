2cbd009cf28c8ed6f49138d574aed618fc51858f
root <zakariarassili@gmail.com>
2023-10-17 13:55:36 +0000

    project files

---
+++ index.js
@@ -0,0 +1,24 @@
+document.getElementById("strbtn").addEventListener("click", function () {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+  var randomDiceImage = "/images/dice" + randomNumber1 + ".png"; // image 1-6
+  var randomDiceImage2 = "/images/dice" + randomNumber2 + ".png"; // image 1-6
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomDiceImage);
+
+  var image2 = document.querySelectorAll("img")[1];
+
+  image2.setAttribute("src", randomDiceImage2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🏆";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+});
+
